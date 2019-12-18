77910ae41489662a7bef45d241b09ea17fda421f
William000000 <sugiraw@gmail.com>
2019-12-18 23:44:32 +0200

    ch(setUpReact): Add initial set up of react app with webpack - Set up  redux [Finishes #170355139]

---
+++ src/redux/actions-types/searchActionsType.js
@@ -0,0 +1,4 @@
+export const SEARCH_START = 'SEARCH_START';
+export const SEARCH_END = 'SEARCH_END';
+export const SEARCH_SUCCESS = 'SEARCH_SUCCESS';
+export const SEARCH_FAILLURE = 'SEARCH_FAILLURE';
